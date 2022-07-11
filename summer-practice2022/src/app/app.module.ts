7c445857848ffb5925c8dcb7a89f88a1535d8e82
Bulboaca, Maria <Maria.Bulboaca@Centric.eu>
2022-07-11 19:15:59 +0300

    Add menu component & page not found

--- summer-practice2022/src/app/app.module.ts
+++ summer-practice2022/src/app/app.module.ts
@@ -4,15 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MenuComponent } from './core/components/menu/menu.component';
+import { RegisterComponent } from './core/components/register/register.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, RegisterComponent, MenuComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
