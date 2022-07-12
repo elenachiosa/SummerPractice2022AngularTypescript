cc3560f00e497c3bc2a822572fcf354e31dacb70
Bulboaca, Maria <Maria.Bulboaca@Centric.eu>
2022-07-12 19:41:24 +0300

    Add routing and forms

--- summer-practice2022/src/app/app-routing.module.ts
+++ summer-practice2022/src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
+import { RegisterComponent } from './core/components/register/register.component';
+import { MyFirstGuardGuard } from './shared/guards/my-first-guard.guard';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/register',
+    pathMatch: 'full',
+  },
+  {
+    path: 'register',
+    canActivate: [MyFirstGuardGuard],
+    component: RegisterComponent,
+  },
+  {
+    path: 'users',
+    loadChildren: () =>
+      import('./features/users/users.module').then(
+        (module) => module.UsersModule
+      ),
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
---
+++ summer-practice2022/src/app/shared/guards/my-first-guard.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MyFirstGuardGuard implements CanActivate {
+  isAdmin = true;
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    //console.log(route, state);
+    return this.isAdmin;
+  }
+}
